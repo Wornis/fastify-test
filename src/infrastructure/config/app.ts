292d9bfe22b3b280f308bb6a0c7c272edce1c57f
Keviinraj BALARATNAM <keviinraj.balaratnam@gmail.com>
2025-07-06 18:45:01 +0200

    feat: add helmet, cors & swagger

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -4,11 +4,41 @@ import awilixPlugin from '../plugins/awilix-plugin'
 import userRoutes from '../../interfaces/routes/user.routes'
 import productRoutes from '../../interfaces/routes/product.routes'
 import orderRoutes from '../../interfaces/routes/order.routes'
+import cors from '@fastify/cors'
+import helmet from '@fastify/helmet'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 
 export async function setupApp(server: FastifyInstance): Promise<FastifyInstance> {
     // Register plugins
     server.register(dbPlugin)
     server.register(awilixPlugin)
+
+    server.register(helmet, {
+        global: true
+    })
+
+    server.register(cors, {
+        origin: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    })
+
+    server.register(swagger, {
+        swagger: {
+            info: {
+                title: 'Fastify API',
+                description: 'API documentation',
+                version: '1.0.0'
+            },
+            host: 'localhost:3000',
+            schemes: ['http'],
+            consumes: ['application/json'],
+            produces: ['application/json']
+        }
+    })
+    server.register(swaggerUi, {
+        routePrefix: '/documentation'
+    })
 
     // Register routes
     server.register(userRoutes, { prefix: '/api' })
